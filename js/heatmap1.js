ff8a8bbee2ffbc21660942e02d87f628a04a9d9e
BlueberryyyInc <148309036+BlueberryyyInc@users.noreply.github.com>
2024-10-19 23:24:10 +1100

    Update heatmap1.js

--- js/heatmap1.js
+++ js/heatmap1.js
@@ -1,4 +1,4 @@
-{
+vegaEmbed('#heatmap-reasons',{
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
   "description": "Heatmap showing reasons for visiting Australia from 2014-2024.",
   "data": {
@@ -33,4 +33,4 @@
       "titleFontSize": 14
     }
   }
-}
+}).catch(console.error);
